a7ee3836db8f4aa648a01f438678ddb0c2720074
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Window1.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+using System.Windows;
+using System.Windows.Input;
+using DevExpress.Xpf.Editors;
+
+namespace GridApplication {
+
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+            DataSet dataSet = new DataSet();
+            dataSet.ReadXml("Cars2.xml");
+            grid.ItemsSource = dataSet.Tables[0];
+        }
+    }
+
+    public class RichTextEdit : TextEdit {
+        protected override bool NeedsKey(Key key, ModifierKeys modifiers) {
+            if (EditMode == EditMode.InplaceActive && key == Key.Enter && 
+                (modifiers & ModifierKeys.Control) == 0)
+                return true;
+
+            return base.NeedsKey(key, modifiers);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to embed a RichTextBox into the DXGrid
+
+
+<p>The following example illustrates how to embed a standard RichTextBox control into DXGrid cells.</p>
+
+<br/>
+
+
